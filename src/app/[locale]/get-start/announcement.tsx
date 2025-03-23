6a4cf2bd69425d69f4ed9ee061162924ed13de78
YoungYa <1829594026@qq.com>
2025-03-23 16:25:58 +0800

    fix:fix the announcement cached

--- src/app/[locale]/get-start/announcement.tsx
+++ src/app/[locale]/get-start/announcement.tsx
@@ -12,50 +12,30 @@ import {
 import ReactMarkdown from "react-markdown";
 import { useTranslations } from "next-intl";
 import { closeAll, addToast } from "@heroui/toast";
-import { useEffect, useState } from "react";
-import type { ToastProps } from "@heroui/toast/dist/use-toast";
-import {CLIENT_BACKEND} from "@/app/requests/misc";
 
-type Announcement = {
+type PropsType = {
   summary: string
   details: string
 }
 
-export default function Announcement({locale = "zh"}: { locale: "zh" | "en" }) {
+export default function Announcement({details, summary}: PropsType) {
   const t = useTranslations("Component.Announcement");
   // model state
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
-  const [announcement, setAnnouncement] = useState<Announcement>();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-  const getAnnouncementInfo = async () => {
-    try {
-      const response = await fetch(`${CLIENT_BACKEND}/api/misc/anno?lang=${locale}`);
-      const announcement = await response.json()
-
-      closeAll();
-      addToast({
-        title: (
-          <div onClick={onOpen}
-               className={'cursor-pointer w-full h-full'}
-          >{t("newAnnouncement")} - {announcement.data.summary}</div>
-        ),
-        timeout: 60000,
-        classNames: {
-          base: `border-1 before:bg-primary border-primary-200 dark:border-primary-100 hover:bg-primary-100 dark:hover:bg-primary-200 transition-all duration-300`
-        },
-      } as ToastProps);
-      setAnnouncement(announcement.data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  useEffect(() => {
-    getAnnouncementInfo();
-    const timer = setInterval(() => {
-      getAnnouncementInfo();
-    }, 1000 * 60);
-    return () => clearInterval(timer);
-  }, []);
+  closeAll();
+  addToast({
+      title: (
+        <div onClick={onOpen}
+             className={'cursor-pointer w-full h-full'}
+        >{t("newAnnouncement")} - { summary }</div>
+      ),
+      timeout: 60 * 1000,
+      classNames: {
+        base: `border-1 before:bg-primary border-primary-200 dark:border-primary-100 hover:bg-primary-100 dark:hover:bg-primary-200 transition-all duration-300`
+      }
+    } as any
+  )
 
   return (
     <>
@@ -63,9 +43,9 @@ export default function Announcement({locale = "zh"}: { locale: "zh" | "en" }) {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">{announcement?.summary}</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">{ summary }</ModalHeader>
               <ModalBody>
-                <ReactMarkdown>{announcement?.details}</ReactMarkdown>
+                <ReactMarkdown>{ details }</ReactMarkdown>
               </ModalBody>
               <ModalFooter>
                 {/* <Button color="danger" variant="light" onPress={onClose}>

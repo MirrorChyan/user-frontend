3e8eb0e7efae5be902b4f5fc15824e65db1b2e24
hmydgz <hmydgz@gmail.com>
2025-03-26 19:15:10 +0800

    feat: PC端的 更多 按钮跟随 home plans 的最后一个

--- src/app/[locale]/get-start/plans.tsx
+++ src/app/[locale]/get-start/plans.tsx
@@ -3,7 +3,7 @@
 import { Plan } from "@/app/requests/planInfo";
 import { useTranslations } from "next-intl";
 import PlanCard from "./plan-card";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { cn } from "@/lib/utils/css";
 
 type PlansProps = {
@@ -29,16 +29,39 @@ export default function Plans({
     <PlanCard key={plan.planId} plan={plan} {...rest} />
   ))
 
+  const homePlansRef = useRef<HTMLDivElement>(null);
+  const [offset, setOffset] = useState<number | null>(10000);
+
+  useEffect(() => {
+    if (_homePlans.length === 0) return;
+    if(homePlansRef.current){
+      const { left } = homePlansRef.current!.getBoundingClientRect();
+      const { right } = homePlansRef.current?.lastElementChild?.getBoundingClientRect() ?? { right: 0 };
+      if (document.body.clientWidth < 768) {
+        setOffset(null);
+      } else {
+        setOffset(right - left);
+      }
+    }
+  }, [homePlansRef.current, _homePlans]);
+
   return (
     <div className="mx-auto max-w-md md:max-w-2xl lg:max-w-4xl xl:mx-0 xl:max-w-6xl self-center">
-      <div className={cn(
-        'isolate self-center px-2 relative',
-        'grid grid-cols-1 gap-8 md:grid-cols-3',
-      )}>
-        {renderPlans(_homePlans)}
+      <div className="relative">
+        <div ref={homePlansRef} className={cn(
+          'isolate self-center px-2',
+          'grid grid-cols-1 gap-8 md:grid-cols-3',
+        )}>
+          {renderPlans(_homePlans)}
+        </div>
         { _morePlans.length ? (
           <button
-            className="absolute left-full bottom-10 text-nowrap text-indigo-600 hover:text-indigo-500 translate-x-0 md:translate-x-6"
+            className={cn(
+              'absolute -bottom-9 left-1/2 -translate-x-1/2 z-10',
+              'md:left-auto md:translate-x-6 md:bottom-10',
+              'text-indigo-600 hover:text-indigo-500 text-nowrap',
+            )}
+            style={offset ? { left: offset } : {}}
             onClick={() => setIsOpenMore(val => !val)}>
             { isOpenMore ? t('hide') : t('more') }
           </button>
@@ -51,7 +74,7 @@ export default function Plans({
         )}>
           <div className="min-h-0">
             <div className={cn(
-              'isolate self-center p-2 pt-8',
+              'isolate self-center p-2 md:pt-8 pt-12',
               'grid grid-cols-1 gap-8 md:grid-cols-3',
             )}>
               {renderPlans(_morePlans)}
